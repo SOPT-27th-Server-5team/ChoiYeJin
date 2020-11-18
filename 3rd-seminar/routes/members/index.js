1427e91c85470afef5653b16a8664ab63c437974
Yejin6911 <cdjin6911@gmail.com>
2020-11-18 16:15:05 +0900

    3차 세미나 과제 Level1

--- 3rd-seminar/routes/members/index.js
+++ 3rd-seminar/routes/members/index.js
@@ -41,6 +41,7 @@ router.get('/:idx', (req, res) => {
     const member = membersDB.find(member => member.idx == idx);
 
     if (member === undefined) {
+        console.log('id가 유효하지 않습니다.')
         return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NO_USER));
     }
     return res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.MEMBER_READ_SUCCESS, member));
--- 3rd-seminar/routes/ranking/index.js
+++ 3rd-seminar/routes/ranking/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', function(req, res, next){
-    res.render('index',{ title: 'Express' });
+router.get('/popular', function(req, res, next){
+    res.status(200).send('인기많은순');
+});
+
+router.get('/bestreply', (req, res) => {
+    res.status(200).send('댓글많은순');
+});
+
+router.get('/age', (req, res) => {
+    res.status(200).send('연령별');
 });
 
 module.exports = router;
