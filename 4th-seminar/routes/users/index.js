16994e500cfda4f00da13cd4f20bd702c580b172
Yejin6911 <cdjin6911@gmail.com>
2020-11-19 18:23:10 +0900

    4차 세미나 과제 Level3

--- 4th-seminar/routes/users/index.js
+++ 4th-seminar/routes/users/index.js
@@ -129,5 +129,60 @@ router.get('/:id', async (req, res) => {
     }
 })
 
+router.delete('/:id', async (req, res) => {
+    const { id } = req.params;
+    try{
+        const user = await User.findOne({
+            where: {
+                id: id,
+            }
+        });
+        if (!user){
+            console.log('존재하지 않는 아이디입니다.');
+            return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NO_USER));            
+        }
+        const result = await User.destroy({
+            where: {
+                id: id,
+            }
+        });
+        return res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.DELETE_USER_SUCCESS));
+    }
+    catch(error) {
+        console.error(error);
+        return res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.DELETE_USER_FAIL));
+    }
+})
+
+router.put('/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { email, userName } = req.body;
+
+        const user = await User.findOne({
+            where: {
+                id: id,
+            }
+        });
+        if (!user) {
+            console.log('존재하지 않는 아이디 입니다.');
+            return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NO_USER));
+        }
+        const result = await User.update({
+            email: email,
+            userName: userName
+        }, {
+            where: {
+              id: id
+            }
+          });
+          return res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.UPDATE_USER_SUCCESS));
+
+    }
+    catch(error) {
+        console.error(error);
+        return res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.UPDATE_USER_FAIL));
+    }
+})
 
 module.exports = router;
